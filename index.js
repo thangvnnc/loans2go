cad913d416970792e4332456def2002820710be5
unknown <thangvnnc@gmail.com>
2019-11-22 16:11:46 +0700

    ms

--- index.js
+++ index.js
@@ -1,19 +1,158 @@
+const DBInfo = "./Info.db";
+const PORT = process.env.PORT || 9999;
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let jwt = require('jsonwebtoken');
 let fs = require('fs');
 let path = require('path');
-const PORT = process.env.PORT || 9999;
+let sqlite3 = require('sqlite3').verbose();
+let session = require('express-session');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'ABCDEFGHIJK',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
-app.listen(PORT, function(error) {
-    if (error) {
-        console.log(error);
+app.listen(PORT, function(err) {
+    if (err) {
+        writeLog(err);
         return;
     };
+
+    initDatabaseSQLite();
     console.log("Server running...");
 });
+
+app.get('/login', function (req, res) {
+    res.redirect('login.html');
+});
+
+app.post('/login', function (req, res) {
+    var post = req.body;
+    if (post.username === 'tien' && post.password === '0978248835ox') {
+        let sessionUser = {
+            username: post.username,
+            password: post.password,            
+        }
+        req.session.sessionUser = sessionUser;
+        res.redirect('/admin');
+    } else {
+      res.send('Bad user/pass');
+    }
+});
+
+function checkAuth(req, res, next) {
+    if (!req.session.sessionUser) {
+        res.redirect('login.html');
+    } else {
+        next();
+    }
+}
+
+app.use('/admin', checkAuth, function(req, res) {
+    res.redirect('admin.html');
+});
+
+app.post('/apply', function(req, res) {
+    let data = req.body;
+    let name = data['name'];
+    let phone = data['phone'];
+    let amount = data['amount'];
+    if (name == ''|| phone == '' || amount == '') {
+        res.send({error: "Lỗi hệ thống vui lòng liên hệ (+84) 035 260 8118"});
+        return;
+    }
+
+    let customer = {
+        name : name,
+        phone : phone,
+        amount : amount,
+    }
+
+    insertCustomer(customer, function(err) {
+        if (err) {
+            res.send({error: "Lỗi hệ thống vui lòng liên hệ (+84) 035 260 8118"});
+            return;
+        } 
+        res.send({error: "OK"});
+    });
+});
+
+function insertCustomer(customer, fn) {
+    connect(function(db) {
+        let stmt = db.prepare("INSERT INTO Customers (name, phone, amount)  VALUES (?, ?, ?)", function(err) {
+            if (err) {
+                fn(err);
+                writeLog("Error prepare INSERT INTO Customers");
+                writeLog(err);                
+                return;
+            }
+
+            stmt.run([customer.name, customer.phone, customer.amount], function(err) {
+                if (err) {
+                    fn(err);
+                    writeLog("Error run INSERT INTO Customers");
+                    writeLog(err);
+                    return;
+                }
+
+                fn(null);    
+            });  
+        });
+             
+    });
+}
+
+function writeLog(message) {
+    message += '';
+    fs.appendFileSync("./log.txt", message + "\n");
+}
+
+function initDatabaseSQLite() {
+    connect(function(db) {
+        db.run("CREATE TABLE IF NOT EXISTS Customers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, amount REAL)",
+        function (err) {
+            if (err) {
+                db.close();
+                writeLog(err);
+                return;
+            }
+            db.close();
+        });        
+    })
+}
+
+function connect(fn) {
+    let db = new sqlite3.Database(DBInfo, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE
+        , (err) => {
+            if (err) {
+                writeLog(err);
+                return;
+            }
+    
+            db.serialize(function() {
+                fn(db);
+            });
+        });
+}
+
+
+
+    // var db = new sqlite3.Database(':memory:');
+    // db.serialize(function() {
+    //     db.run("CREATE TABLE IF NOT EXISTS Customers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, amount REAL, phone TEXT)");
+    //     var stmt = db.prepare("INSERT INTO Customers (name, amount, phone)  VALUES (?, ?, ?)");
+    //     for (var i = 0; i < 10; i++) {
+    //         stmt.run("a " + i, "a " + i, "b " + i);
+    //     }
+    //     stmt.finalize();
+    //     db.each("SELECT id, name FROM Customers", function(err, row) {
+    //         console.log(row.id + ": " + row.name);
+    //     });
+    // });
--- public/js/main.js
+++ public/js/main.js
@@ -133,6 +133,142 @@ $(window).on('load', function() {
 		}
 
 	});
+	/*------------------------------Tự code---------------------------------*/
+	/*------------------
+		Modal
+	--------------------*/
+	function showModal(message) {
+		let html = "";
+		let time = new Date().getTime();
+		let classModal = "modalError-" + time;
+		html += '<div class="modal fade ' + classModal + '" role="dialog">';
+		html += '<div class="modal-dialog" style="width: 250px;">';
+		html += '<div class="modal-content">';
+		html += '<div class="modal-body">';
+		html += '<p id="error">' + escapeHtml(message) + '</p>';
+		html += '</div>';
+		html += '<div class="modal-footer">';
+		html += '<button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>';
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+		$('body').append(html);
+		$('body').find('.' + classModal).modal();
+	
+		$('body').on('hidden.bs.modal', '.' + classModal, function (e) {
+		    $('body').find('.' + classModal).remove();
+		    return false;
+		});
+	}
+	
+	/*------------------
+		Modal confirm
+	--------------------*/
+	var _cbComfirm = [];
+	function showModalConfirm(message, callback) {
+		let time = new Date().getTime();
+		_cbComfirm[time] = callback;
+		let classModal = "modalConfirm-" + time;
+		let html = "";
+		html += '<div class="modal fade ' + classModal + '" role="dialog" data-backdrop="static" data-keyboard="false">';
+		html += '<div class="modal-dialog" style="width: 250px;">';
+		html += '<div class="modal-content">';
+		html += '<div class="modal-body">';
+		html += '<p id="error">' + escapeHtml(message) + '</p>';
+		html += '</div>';
+		html += '<div class="modal-footer">';
+		html += '<button type="button" class="btn btn-primary btn-ok">OK</button>';
+		html += '<button type="button" class="btn btn-danger btn-cancel">Cancel</button>';
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+		$('body').append(html);
+		$('body').find('.' + classModal).modal();
+	
+		$('body').on('click', '.' + classModal + ' .btn-ok', function (e) {
+			if (_cbComfirm[time] != null) {
+				_cbComfirm[time](true);
+			}
+		    $('body').find('.' + classModal).modal('hide');
+		    return false;
+		});
+	
+		$('body').on('click', '.' + classModal + ' .btn-cancel', function (e) {
+			if (_cbComfirm[time] != null) {
+				_cbComfirm[time](false);
+			}
+		    $('body').find('.' + classModal).modal('hide');
+		    return false;
+		});
+	
+		$('body').on('hidden.bs.modal', '.' + classModal + '', function (e) {
+		    _cbComfirm[time] = null;
+		    $('body').find('.' + classModal + '').remove();
+		    return false;
+		});
+	}
 
+	/*------------------
+		Escape html
+	--------------------*/
+	function escapeHtml(unsafe) {
+		unsafe = unsafe + '';
+		return unsafe
+			.replace(/&/g, "&amp;")
+			.replace(/</g, "&lt;")
+			.replace(/>/g, "&gt;")
+			.replace(/"/g, "&quot;")
+			.replace(/'/g, "&#039;");
+	}
+
+	/*------------------
+		Event click apply
+	--------------------*/
+	$('.btn-apply').on('click', function(e) {
+		let name = $('.ip-name').val();
+		let phone = $('.ip-phone').val();
+		let amount = $('.sl-amount').val();
+		if (name == '') {
+			showModal('Vui lòng nhập họ và tên');
+			$('.ip-name').focus();
+			return false;
+		}
+
+		if (phone == '') {
+			showModal('Vui lòng nhập số điện thoại');
+			$('.ip-phone').focus();
+			return false;
+		}
+
+		if (amount == '') {
+			showModal('Vui lòng chọn khoản vay');
+			$('.sl-amount').focus();
+			return false;
+		}
+
+		let dataPost = {
+			name : name,
+			phone : phone,
+			amount : amount,
+		}
+		
+		$.ajax({
+            url: '/apply',
+            type: 'post',
+			dataType: 'json',
+			data: dataPost,
+            success: function (result) {
+				if (result.error != "OK") {
+					showModal(err);
+					return;
+				}
+				showModal("Đã gửi hồ sơ, chúng sẽ sẽ sớm liên hệ tới quí khách. Xin cảm ơn!");
+				return;
+            },
+        });
+		return false;
+	});
 })(jQuery);
 
